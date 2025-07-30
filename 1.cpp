47ef2f933a991a53e810c3e00d1f41ffb3d2d390
Arlan Tengga <arlantengga8@gmail.com>
2025-07-30 17:14:16 +0700

    1

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+
+int main(){
+    int result = 0;
+    for(int i=0;i<1000;i++){
+        if(i % 3 == 0 || i % 5 == 0){
+            result += i;
+        }
+    }
+
+    std::cout<<result<<std::endl;
+}
