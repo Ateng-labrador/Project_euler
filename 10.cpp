ee823335cc3608ddf27d4d3f2515dcd0799f5027
Arlan Tengga <arlantengga8@gmail.com>
2025-07-31 20:54:36 +0700

    7

--- 10.cpp
+++ 10.cpp
@@ -2,6 +2,22 @@
 #include <cmath>
 #include <vector>
 
+/**
+ * Angka Prima adalah angka yang habsi dibagi 1 dan dirinya sendiri
+ * Jika suatu angka P adalah prima,maka semua kelipatan dari P(mulai dari p^2) pasti bukan prima
+ * jadi kelipatan dari angka prima bisa di buang
+ * 
+ * step-step Algoritma
+ * buat boolean sepanjang n+1 dan isinya true
+ * Semua bilangan komposit(bukan prima) pasti punya faktor kecil yang =< sqrt(n)
+ * jika newboolean[i] == true makan untuk kelipatanya akan bersipat false
+ * 
+ */
+
+/**
+ * 0 1 tidak dimasukkan karna bukan angak prima
+ */
+
 std::vector<int> prime_number(int n){
     std::vector<bool> newboolean(n+1,true);
     for(int i=2;i<=sqrt(n);i++){
---
+++ 7.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+// std::vector<int> prime_number(int n){
+//     std::vector<bool> is_prime(n+1,true);
+//     for(int i=2;i<=sqrt(n);i++){
+//         if(is_prime[i]==true){
+//             for(int j=i*i;j<=n;j+=i){
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+//     std::vector<int> res;
+//     for(int p=2;p<=10001;p++){
+//         if(is_prime[p]){
+//             res.push_back(p);
+//         }
+//     }
+//     return res;
+// }
+
+// void print(std::vector<int>&res){
+//     for(auto x:res){
+//         std::cout<<x<<std::endl;
+//     }
+// }
+
+// while (counter<10001){
+    //     if(is_prime(x)){
+    //         counter++;
+    //     }
+    //     x+=2;
+    // }
+
+// 104743
+
+bool is_prime(int n){
+    if(n<2){
+        return false;
+    }
+    for(int i=2;i<=sqrt(n);i++){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main(){
+    int x = 1;
+    int count = 0;
+    for(int i=0;count<10001;x++){
+        if(is_prime(x)){
+            count++;
+        }
+    }
+
+    std::cout<<x-1<<std::endl;
+    return 0;
+}
