f56cea7e268229e1bc4ac1d555f2b226987217a6
Arlan Tengga <arlantengga8@gmail.com>
2025-08-04 11:55:55 +0700

    pake string

---
+++ 20_3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+void product(std::string& str,int N){
+    int len = str.size();
+    int reminder = 0;
+    for(int i=0;i<len;i++){
+        int temp = (str[len-1-i]-'0')*N+reminder;
+        str[len-1-i] = char(temp%10 + '0');
+        reminder = temp/10;
+    }
+    if(reminder){
+        str = std::to_string(reminder)+str;
+    }
+}
+
+std::string factorial(int N){
+    std::string fac = "1";
+    int counter = 1;
+    while(counter < N){
+        counter++;
+        product(fac,counter);
+    }
+    return fac;
+}
+
+int sum_string(std::string str){
+    int sum = 0;
+    for(char c:str){
+        sum += (c - '0');
+    }
+    return sum;
+}
+
+int main(){
+    std::cout<<factorial(5)<<std::endl;
+    return 0;
+}
