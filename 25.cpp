7735e443a6842efdbf6bfc44d1f014612cbb57f0
Arlan Tengga <arlantengga8@gmail.com>
2025-08-07 13:28:15 +0700

    improsisasi

--- 25.cpp
+++ 25.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <string>
 using ll = long long;
+
 
 std::map<int,std::vector<ll>> Fibonanci(ll n){
     std::map<int,std::vector<ll>>Fib_list = {
         {1,{1}},
         {2,{1}}
     };
-    // for(int i=2;i<n;i++){
-    //     ll x = Fib_list[i-1]+Fib_list[i-2];
-    for(int i=3;i<=n;++i){
-        ll x = Fib_list[i-1][0]+Fib_list[i-2][0];
-        Fib_list.insert({i,{x}});
+    // ll cout = 3;
+    // for(int i=3;i<=cout;++i){
+    //     ll x = Fib_list[i-1][0]+Fib_list[i-2][0];
+    //     std::string A = std::to_string(x);
+    //     if(A.size() >= n){
+    //         Fib_list.insert({i,{x}});
+    //     }
+    // }
+    // cout++;
+
+    ll cout = 3;
+    while (true)
+    {
+        ll x = Fib_list[cout-1][0]+Fib_list[cout-2][0];
+        std::string A = std::to_string(x);
+        Fib_list[cout] = {x};
+        if(A.size()>=n){
+            break;
+        }
+        ++cout;
     }
+    
     return Fib_list;
 }
 
@@ -31,3 +49,6 @@ int main(){
     std::map<int,std::vector<ll>> A = Fibonanci(3);
     print(A);
 }
+
+// for(int i=2;i<n;i++){
+    // //     ll x = Fib_list[i-1]+Fib_list[i-2];
