25b1b637383186c8ca4da1e732a8735a91f260fc
Arlan Tengga <arlantengga8@gmail.com>
2025-08-12 13:46:55 +0700

    menambahkan python 3

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 for i in range(1,10):
     if i % 2 != 0:
         continue
-    print(i)
+    
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+
+print(factorial(5))
